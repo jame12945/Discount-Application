6c1982c0a80bd004947ed31b2e10dd9110e9240c
jame12945 <jame12945@gmail.com>
2024-04-02 02:17:53 +0700

    working process 10%

---
+++ lib/features/discount/presentation/pages/discount_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DiscountPage extends StatefulWidget {
+  const DiscountPage({super.key});
+
+  @override
+  State<DiscountPage> createState() => _DiscountPageState();
+}
+
+class _DiscountPageState extends State<DiscountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Apply Discount')));
+  }
+}
